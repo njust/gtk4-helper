2cc9f1c7ffe788e6c07c481857eaa78fb6b1f985
nico.just <mail@njust.de>
2021-09-17 22:46:07 +0200

    Added init method

--- gtk4-helper/src/widget.rs
+++ gtk4-helper/src/widget.rs
@@ -15,10 +15,20 @@ pub struct WidgetContainer<W: Widget> {
 
 impl<W: Widget> WidgetContainer<W> {
     pub fn new<T: MsgHandler<W::Msg> + Clone>(sender: T, input: Option<W::Input>) -> WidgetContainer<W> {
+        let mut widget = W::create(sender.clone(), input);
+        widget.init(sender.clone());
         Self {
-            widget: Box::new(W::create(sender.clone(), input)),
+            widget: Box::new(widget),
             tx: Rc::new(sender.clone())
         }
+    }
+
+    pub fn get_mut(&mut self) -> &mut Box<W> {
+        &mut self.widget
+    }
+
+    pub fn get(&self) -> &Box<W> {
+        &self.widget
     }
 
     pub fn update(&mut self, msg: W::Msg) {
@@ -53,6 +63,9 @@ pub trait Widget: Sized + 'static {
 
     fn new_with_data<T: MsgHandler<Self::Msg> + Clone>(sender: T, input: Self::Input) -> WidgetContainer<Self> {
         WidgetContainer::<Self>::new(sender, Some(input))
+    }
+
+    fn init<T: MsgHandler<Self::Msg> + Clone>(&mut self, _: T) {
     }
 
     fn update(&mut self, msg: Self::Msg) -> Command<Self::Msg>;
